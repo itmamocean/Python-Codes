07ad3b8904d7d69609e282a16a383eef761b3536
itmamocean <113386945+itmamocean@users.noreply.github.com>
2022-10-05 21:24:46 +0600

    Add files via upload

---
+++ prog2.py
@@ -0,0 +1,3 @@
+import trial
+print("Hello I am inside prog2.py!")
+print(trial.__name__)
---
+++ program.py
@@ -0,0 +1,7 @@
+import new_fibo
+
+print("Hello, I am inside program.py!")
+x = int(input("Enter n number of fibonacci numbers: "))
+n = new_fibo.find_fib(x)
+
+print("nth fibonacci number is, ", n)
